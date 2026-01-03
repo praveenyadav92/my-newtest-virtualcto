fd20e02812d5d30b32aab638db5de64582cec580
Praveen Yadav <74948334+praveenyadav92@users.noreply.github.com>
2026-01-03 13:54:09 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.scss'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
+import { Providers } from './providers'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  )
+}
